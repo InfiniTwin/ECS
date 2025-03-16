0a3beac6388dfbf17ad5b8260fcd27b9ed385632
Cuku <xhikab@gmail.com>
2025-03-16 21:51:21 +0100

    Refactor

--- Source/ThirdParty/FlecsLibrary/FlecsLibrary.Build.cs
+++ Source/ThirdParty/FlecsLibrary/FlecsLibrary.Build.cs
@@ -10,9 +10,8 @@ public class FlecsLibrary : ModuleRules
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core" });
 
-		//The path for the header files
 		PublicIncludePaths.AddRange(new string[] { "FlecsLibrary/Public" });
-		//The path for the source files
+
 		PrivateIncludePaths.AddRange(new string[] { "FlecsLibrary/Private" });
 
 		if (Target.Platform != UnrealTargetPlatform.Win64) AppendStringToPublicDefinition("flecs_EXPORTS", "0");
