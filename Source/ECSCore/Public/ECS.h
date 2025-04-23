fad6222f28b883d86d1f172c14e86c99390c0635
Besjan <xhikab@gmail.com>
2025-04-23 10:39:02 +0200

    Unify json deserialization

--- Source/ECSCore/Public/ECS.h
+++ Source/ECSCore/Public/ECS.h
@@ -16,53 +16,60 @@ constexpr const char* Member(const char* str) {
 }
 #define MEMBER(str) Member(#str)
 
+constexpr const char* SingletonsField = "singletons";
 constexpr const char* EntitiesField = "entities";
 constexpr const char* ParentField = "parent";
 constexpr const char* ChildrenField = "children";
 
 struct ECS {
 public:
-	static inline void SingletonsFromAsset(flecs::world& world, const FString name) {
-		auto data = Assets::LoadJsonAsset(name);
-		auto singletonsEntity = world.entity().disable();
-		singletonsEntity.from_json(data);
-		free(data);
-
-		singletonsEntity.each([&](flecs::id id) {
-			const ecs_type_info_t* info = ecs_get_type_info(world, id);
-			const void* ptr = singletonsEntity.get(id);
-			ecs_set_id(world, world.entity(id), id, info->size, ptr);
-			});
-
-		singletonsEntity.destruct();
-	}
-
-	static inline void EntitiesFromAsset(flecs::world& world, const FString name) {
+	static inline void FromJsonAsset(flecs::world& world, const FString name) {
 		auto data = Assets::LoadJsonAsset(name);
 		TSharedRef<TJsonReader<>> reader = TJsonReaderFactory<>::Create(data);
 		free(data);
 
-		// Create entities
 		TSharedPtr<FJsonObject> rootObject;
-		if (FJsonSerializer::Deserialize(reader, rootObject) && rootObject.IsValid()) {
-			const TArray<TSharedPtr<FJsonValue>>* entities = nullptr;
-			if (rootObject->TryGetArrayField(EntitiesField, entities)) {
-				int32 index = 0;
-				for (const TSharedPtr<FJsonValue>& entity : *entities) {
-					FString entityJsonString;
-					FJsonSerializer::Serialize(entity->AsObject().ToSharedRef(),
-						TJsonWriterFactory<>::Create(&entityJsonString));
-					std::string entityJson = TCHAR_TO_UTF8(*entityJsonString);
-					world.entity().from_json(entityJson.c_str());
-					index++;
-				}
+		if (!FJsonSerializer::Deserialize(reader, rootObject) || !rootObject.IsValid())
+			return;
+
+		// Create singletons
+		const TSharedPtr<FJsonObject>* singletons = nullptr;
+		if (rootObject->TryGetObjectField(SingletonsField, singletons)) {
+			FString singletonsJsonString;
+			FJsonSerializer::Serialize(singletons->ToSharedRef(),
+				TJsonWriterFactory<>::Create(&singletonsJsonString));
+			std::string singletonsJson = TCHAR_TO_UTF8(*singletonsJsonString); 
+			
+			auto singletonsEntity = world.entity().disable();
+			singletonsEntity.from_json(singletonsJson.c_str());
+
+			singletonsEntity.each([&](flecs::id id) {
+				const ecs_type_info_t* info = ecs_get_type_info(world, id);
+				const void* ptr = singletonsEntity.get(id);
+				ecs_set_id(world, world.entity(id), id, info->size, ptr);
+				});
+
+			singletonsEntity.destruct();
+		}
+
+		// Create entities
+		const TArray<TSharedPtr<FJsonValue>>* entities = nullptr;
+		if (rootObject->TryGetArrayField(EntitiesField, entities)) {
+			int32 index = 0;
+			for (const TSharedPtr<FJsonValue>& entity : *entities) {
+				FString entityJsonString;
+				FJsonSerializer::Serialize(entity->AsObject().ToSharedRef(),
+					TJsonWriterFactory<>::Create(&entityJsonString));
+				std::string entityJson = TCHAR_TO_UTF8(*entityJsonString);
+				world.entity().from_json(entityJson.c_str());
+				index++;
 			}
 		}
 
 		// Create ChildOf relationships
-		const TArray<TSharedPtr<FJsonValue>>* childOfArray = nullptr;
-		if (rootObject->TryGetArrayField(MEMBER(flecs::ChildOf), childOfArray)) {
-			for (const TSharedPtr<FJsonValue>& childOfValue : *childOfArray) {
+		const TArray<TSharedPtr<FJsonValue>>* childOfRelationships = nullptr;
+		if (rootObject->TryGetArrayField(MEMBER(flecs::ChildOf), childOfRelationships)) {
+			for (const TSharedPtr<FJsonValue>& childOfValue : *childOfRelationships) {
 				const TSharedPtr<FJsonObject>* childOfObject;
 				childOfValue->TryGetObject(childOfObject);
 
