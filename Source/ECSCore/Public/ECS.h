937db39c3d35a9ece0044fd29caee09326017477
Besjan <xhikab@gmail.com>
2025-05-24 23:19:47 +0200

    Not always prefab instances are recognized for deep prefab relations

--- Source/ECSCore/Public/ECS.h
+++ Source/ECSCore/Public/ECS.h
@@ -22,6 +22,30 @@ namespace ECS {
 	constexpr const char* SingletonsField = "singletons";
 	constexpr const char* EntitiesField = "entities";
 
+    static inline void GetInstances(flecs::world& world, flecs::entity prefab, TArray<flecs::entity>& instances)
+    {
+        TQueue<flecs::entity> queue;
+        queue.Enqueue(prefab);
+        TSet<uint64> seen;
+
+        while (!queue.IsEmpty()) {
+            flecs::entity current;
+            queue.Dequeue(current);
+
+            world.each(world.pair(flecs::IsA, current), [&](flecs::entity instance) {
+                if (instance.has(flecs::Prefab) && instance.is_a(prefab))
+                    queue.Enqueue(instance);
+                else {
+                    uint64 id = instance.id();
+                    if (!seen.Contains(id)) {
+                        seen.Add(id);
+                        instances.Add(instance);
+                    }
+                }
+                });
+        }
+    }
+
 	ECSCORE_API void FromJsonAsset(flecs::world& world, const FString name, const FString scope = TEXT(""));
 
 	ECSCORE_API FString AddScope(const FString& in, const FString& scope = TEXT(""));
